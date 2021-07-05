1fa2074ea02543cfbdebf08d92ae52be756f7651
sathyadevops <sathyadevops1@gmail.com>
2021-07-05 03:11:12 +0000

    new code

---
+++ demo.java
@@ -0,0 +1,13 @@
+class Demo
+{
+  public static void main(...)
+  {
+    system.out.print("Hello world");
+    system.out.print("Hello world");
+    system.out.print("Hello world");
+    system.out.print("Hello world");
+    system.out.print("Hello world");
+
+  }
+
+}
