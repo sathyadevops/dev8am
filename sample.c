d673cf35e32218e009ab7f2954fba84af13c0d90
sathyadevops <sathyadevops9@gmail.com>
2024-03-13 03:20:52 +0000

    c-code

---
+++ sample.c
@@ -0,0 +1,8 @@
+void main()
+{
+  printf(" HELLO WORLD, GOOD DAY! ");
+  printf(" HELLO WORLD, GOOD DAY! ");
+  printf(" HELLO WORLD, GOOD DAY! ");
+  printf(" HELLO WORLD, GOOD DAY! ");
+  printf(" HELLO WORLD, GOOD DAY! ");
+}
